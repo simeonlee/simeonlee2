622900aa59a6241760d7377929278f0b2ab25c6b
Simeon Lee <simeonhlee@gmail.com>
2016-11-18 02:12:30 -0800

    Add http server code

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 // var passport = require('passport');
 // var https = require('https');
-// var http = require('http');
+var http = require('http');
 // var fs = require('fs');
 var port = process.env.NODE_ENV === 'development' ? 5000 : 3000;
 
@@ -10,8 +10,29 @@ var port = process.env.NODE_ENV === 'development' ? 5000 : 3000;
 require('./config/middleware.js')(app, express); // Middleware
 // require('./config/routes.js')(app); // Routes
 
-app.listen(port, () => {
-  console.log('Listening on Port: ' + port);
-});
+if (process.env.NODE_ENV === 'production') {
+
+  // var privateKey = fs.readFileSync('./https/www_yourjourney_io.key');
+  // var certificate = fs.readFileSync('./https/www_yourjourney_io.crt');
+  // var pemBundle = fs.readFileSync('./https/www_yourjourney_io_bundle.pem');
+
+  // serverOptions = {
+  //   ca: pemBundle,
+  //   key: privateKey,
+  //   cert: certificate
+  // }
+
+  // sequelize.sync().then(function() {
+    // console.log('Synced with mySQL through Sequelize.');
+    // https.createServer(serverOptions, app).listen(3000);
+    http.createServer(app).listen(1337);
+    // console.log('Listening on Port: 3000');
+  // });
+
+} else {
+  app.listen(port, () => {
+    console.log('Listening on Port: ' + port);
+  });
+}
 
 module.exports = app;
