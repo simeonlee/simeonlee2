124885ba8d813a43a996eb279857560ae8797f2a
Simeon Lee <simeonhlee@gmail.com>
2016-11-18 02:37:47 -0800

    Delete http code

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ var port = process.env.NODE_ENV === 'development' ? 5000 : 3000;
 require('./config/middleware.js')(app, express); // Middleware
 // require('./config/routes.js')(app); // Routes
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
 
   // var privateKey = fs.readFileSync('./https/www_yourjourney_io.key');
   // var certificate = fs.readFileSync('./https/www_yourjourney_io.crt');
@@ -25,14 +25,14 @@ if (process.env.NODE_ENV === 'production') {
   // sequelize.sync().then(function() {
     // console.log('Synced with mySQL through Sequelize.');
     // https.createServer(serverOptions, app).listen(3000);
-    http.createServer(app).listen(3000);
+    // http.createServer(app).listen(3000);
     // console.log('Listening on Port: 3000');
   // });
 
-} else {
+// } else {
   app.listen(port, () => {
     console.log('Listening on Port: ' + port);
   });
-}
+// }
 
 module.exports = app;
