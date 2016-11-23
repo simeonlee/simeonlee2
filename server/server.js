e75095b2ea8daee2e00321fd8ebd458ddf9f25a4
Simeon Lee <simeonhlee@gmail.com>
2016-11-23 11:50:21 -0800

    Update server.js code to reflect correct http port listening procedure

--- server/server.js
+++ server/server.js
@@ -1,44 +1,47 @@
 var express = require('express');
 var app = express();
+var http = require('http');
+var port = process.env.NODE_ENV === 'development' ? 5000 : 3000;
+app.set('port', port);
+var server = http.createServer(app);
+server.listen(port);
 // var passport = require('passport');
 // var https = require('https');
-var http = require('http');
 // var fs = require('fs');
-var port = process.env.NODE_ENV === 'development' ? 5000 : 3000;
 
 // require('./config/passport.js')(passport); // Authentication
 require('./config/middleware.js')(app, express); // Middleware
 // require('./config/routes.js')(app); // Routes
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
 
-  // var privateKey = fs.readFileSync('./https/www_yourjourney_io.key');
-  // var certificate = fs.readFileSync('./https/www_yourjourney_io.crt');
-  // var pemBundle = fs.readFileSync('./https/www_yourjourney_io_bundle.pem');
+//   // var privateKey = fs.readFileSync('./https/www_yourjourney_io.key');
+//   // var certificate = fs.readFileSync('./https/www_yourjourney_io.crt');
+//   // var pemBundle = fs.readFileSync('./https/www_yourjourney_io_bundle.pem');
 
-  // serverOptions = {
-  //   ca: pemBundle,
-  //   key: privateKey,
-  //   cert: certificate
-  // }
+//   // serverOptions = {
+//   //   ca: pemBundle,
+//   //   key: privateKey,
+//   //   cert: certificate
+//   // }
 
-  // sequelize.sync().then(function() {
-    // console.log('Synced with mySQL through Sequelize.');
-    // https.createServer(serverOptions, app).listen(3000);
-    // http.createServer(app).listen(3000);
-    // console.log('Listening on Port: 3000');
-  // });
+//   // sequelize.sync().then(function() {
+//     // console.log('Synced with mySQL through Sequelize.');
+//     // https.createServer(serverOptions, app).listen(3000);
+//     // http.createServer(app).listen(3000);
+//     // console.log('Listening on Port: 3000');
+//   // });
 
-  http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World\n");
-    response.close();
-  }).listen(8000);
+//   http.createServer(function(request, response) {
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write("Hello World\n");
+//     response.close();
+//   }).listen(8000);
 
-} else {
-  app.listen(port, () => {
-    console.log('Listening on Port: ' + port);
-  });
-}
+// } else {
+//   app.listen(port, () => {
+//     console.log('Listening on Port: ' + port);
+//   });
+// }
 
 module.exports = app;
